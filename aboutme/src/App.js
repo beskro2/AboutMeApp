c9cda9547d9c1a5284c76cc4a3a30282f3e5751f
beskro2 <125532605+beskro2@users.noreply.github.com>
2024-03-15 00:05:20 -0500

    hamburger menue work

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
 import myImage from './IMG_2196 (1).jpeg'
+
+function toggleMenu(){
+  var x = document.getElementById("hamburgerNav-links");
+  if(x.style.display === 'block') {
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+
 function App() {
-  
   return (
-    
+ 
     <div className="App">
       <nav id ="desktop-nav">
       <div className="menu-container">
@@ -26,16 +36,16 @@ function App() {
         <div className='title'>
           <t>Ben Eskro</t>
         </div>
-        <div className="hamburger-icon" onClick = "toggleMenu()">
-          <span></span>
-          <span></span>
-          <span></span>
+        <div className="hamburger-icon" onClick = {toggleMenu}>
+          <span className ="burderSymbol">---</span>
+          <span className ="burderSymbol">---</span>
+          <span className ="burderSymbol">---</span>
         </div>
-        <div className="menu-links">
-          <li><a href='#about' onClick = "toggleMenu()">about</a></li>
-          <li><a href='#experience' onClick = "toggleMenu()">experience</a></li>
-          <li><a href='#projects' onClick = "toggleMenu()">projects</a></li>
-          <li><a href='#contact' onClick = "toggleMenu()">contact</a></li>
+        <div id="hamburgerNav-links">
+          <li><a href='#about' onClick = {toggleMenu}>about</a></li>
+          <li><a href='#experience' onClick = {toggleMenu}>experience</a></li>
+          <li><a href='#projects' onClick = {toggleMenu}>projects</a></li>
+          <li><a href='#contact' onClick = {toggleMenu}>contact</a></li>
         
         </div>
       </nav>
