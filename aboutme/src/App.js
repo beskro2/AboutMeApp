ba0b4a6c167e6cec12da5f0c1d06087d8e7852ff
beskro2 <125532605+beskro2@users.noreply.github.com>
2024-04-30 18:11:07 -0500

    format project pt1

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -201,7 +201,25 @@ function App() {
           </div>
         </div>
         <div className="projectContainer">
-          <div className="projectCard"></div>
+          <div className="projectCard">
+            {/*move this to a component to call repeatedly*/}
+            <h2>Dungeons & Dragons Web applications</h2>
+            <h3>Description</h3>
+            <p>
+              Created a React web application that allows Dungeons and Dragons
+              players to roll dice virtually as well as share story notes and
+              game items
+            </p>
+            <h3>Goal</h3>
+            <ul>
+              <li>Familurise myself with react framework</li>
+              <li>Practice creating and manipulating components</li>
+              <li>Create solution</li>
+            </ul>
+            <div className="test">
+              <button className="largebtn">github</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
